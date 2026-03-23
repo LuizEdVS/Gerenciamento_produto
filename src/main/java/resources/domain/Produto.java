5be7d0f2cc350dbf99c3252f2c94df3751f828c9
LuizEdVS <luizeduardo1173@gmail.com>
2026-03-22 23:22:27 -0300

    Update Produto.java

---
+++ src/main/java/resources/domain/Produto.java
@@ -0,0 +1,64 @@
+package com.gerenciamento.produto.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity(name = "Produto")
+@Table(name = "tb_produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nm_produto", nullable = false)
+    private String nome;
+
+    @Column(name = "vl_preco", nullable = false)
+    private BigDecimal preco;
+
+    @Column(name = "qt_estoque", nullable = false)
+    private Integer estoque;
+
+    public Produto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
