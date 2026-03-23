c3fa56670bdf1f608de5e23a00d2b8aeef1f3523
LuizEdVS <luizeduardo1173@gmail.com>
2026-03-22 23:24:23 -0300

    Update GerenciamentoProdutoApplication.java

---
+++ src/main/java/resources/service/GerenciamentoProdutoApplication.java
@@ -0,0 +1,12 @@
+package com.gerenciamento.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoProdutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoProdutoApplication.class, args);
+    }
+}
